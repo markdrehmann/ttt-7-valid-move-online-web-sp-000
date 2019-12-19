4e8e0087b0abb518b45a4ecf7f8208727d02663c
Mark Drehmann <markdrehmann@gmail.com>
2019-12-19 22:05:41 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,11 +3,13 @@ def valid_move?(board, index)
   def position_taken?(board, index)
     if board[index] == "" || board[index] == " " || board[index] == nil
       false
-    elsif board[index] == "X" || board[index] == "O"
+    else board[index] == "X" || board[index] == "O"
       true
     end
   end
   if index.between?(0,8) == true && position_taken?(board, index) == false
     true
+  else
+    false
   end
 end
